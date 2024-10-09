1507baaa0bbcb90815da0394639497cd6dd01c18
Jonathan Londoño <londonojonathan343@gmail.com>
2024-10-09 18:58:13 -0500

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,6 @@
+console.log("Bienvenidos al programa de javascript");
+alert ("Tu nombre es:"+ nombre + " tu edad es:"  + edad + " tu ciudad es:" + ciudad )
+var nombre = prompt("Escribe tu nombre:");
+var edad = prompt("Escribe tu edad:");
+var ciudad = prompt("Escribe la ciudad donde vives:");
+console.log("Tu nombre es:"+ nombre + " tu edad es:"  + edad + " tu ciudad es:" + ciudad );
